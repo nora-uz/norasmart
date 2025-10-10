63325d4189eb0ac1c91d39dd554dcb192173701c
jakakhmedov-art <jak.akhmedov@gmail.com>
2025-10-11 01:10:22 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,6 @@
-"use client";
+// app/page.tsx
+import Chat from './components/chat';
 
-import React from "react";
-import styles from "./page.module.css";
-
-const Home = () => {
-  const categories = {
-    "Basic chat": "basic-chat",
-    "Function calling": "function-calling",
-    "File search": "file-search",
-    All: "all",
-  };
-
-  return (
-    <main className={styles.main}>
-      <div className={styles.title}>
-        Explore sample apps built with Assistants API
-      </div>
-      <div className={styles.container}>
-        {Object.entries(categories).map(([name, url]) => (
-          <a key={name} className={styles.category} href={`/examples/${url}`}>
-            {name}
-          </a>
-        ))}
-      </div>
-    </main>
-  );
-};
-
-export default Home;
+export default function Home() {
+  return <Chat />;
+}
