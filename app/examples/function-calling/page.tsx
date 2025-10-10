98cd286757ec618d763bcde1c2d2dbd475893648
jakakhmedov-art <jak.akhmedov@gmail.com>
2025-10-10 16:53:49 +0500

    Update page.tsx

--- app/examples/function-calling/page.tsx
+++ app/examples/function-calling/page.tsx
@@ -38,7 +38,7 @@ const FunctionCalling = () => {
         </div>
         <div className={styles.chatContainer}>
           <div className={styles.chat}>
-            <Chat functionCallHandler={functionCallHandler} />
+            <Chat />
           </div>
         </div>
       </div>
