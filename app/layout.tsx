430d8a377fa648f175eb78a56770d4d81a0345c3
Ai.Pro <jakh.akhmedov@gmail.com>
2025-10-15 11:14:39 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,22 +2,23 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Warnings from "./components/warnings";
 import { assistantId } from "./assistant-config";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Assistants API Quickstart",
-  description: "A quickstart template using the Assistants API with OpenAI",
+  title: "Ждёте малыша? Я помогу!",
+  description: "Nora — искусственный интеллект помощник для беременных.",
   icons: {
-    icon: "/openai.svg",
+    icon: "/banner.webp", // фавикон из public/banner.webp
   },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="ru">
       <body className={inter.className}>
         {assistantId ? children : <Warnings />}
-        <img className="logo" src="/openai.svg" alt="OpenAI Logo" />
+        <img className="logo" src="/banner.webp" alt="Nora AI Logo" />
       </body>
     </html>
   );
